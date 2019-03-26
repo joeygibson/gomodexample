e0f05f4af9cfc8a7669c0f8863fded06389f4f3d
Joey Gibson <joegibso@cisco.com>
2019-03-26 07:27:25 -0400

    initial commit

---
+++ cmd/gomodexample/lib.go
@@ -0,0 +1,23 @@
+package gomodexample
+
+import "github.com/fatih/color"
+
+// PrintInRed prints the given message in red
+func PrintInRed(msg string) {
+	color.Red(msg)
+}
+
+// PrintInGreen prints the given message in green
+func PrintInGreen(msg string) {
+	color.Green(msg)
+}
+
+// PrintInYellow prints the given message in yellow
+func PrintInYellow(msg string) {
+	color.Yellow(msg)
+}
+
+// PrintInBlue prints the given message in blue
+func PrintInBlue(msg string) {
+	color.Blue(msg)
+}
